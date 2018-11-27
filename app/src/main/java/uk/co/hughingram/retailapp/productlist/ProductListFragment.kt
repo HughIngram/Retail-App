2e8eef37c542f63149584a0356b3978f219b53f3
Hugh Ingram <hughi94@Gmail.com>
2018-11-27 23:13:15 +0000

    add list divider and Ripple

--- app/src/main/java/uk/co/hughingram/retailapp/productlist/ProductListFragment.kt
+++ app/src/main/java/uk/co/hughingram/retailapp/productlist/ProductListFragment.kt
@@ -1,7 +1,9 @@
 package uk.co.hughingram.retailapp.productlist
 
 import android.os.Bundle
+import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.View
 import io.reactivex.Observable
 import kotlinx.android.synthetic.main.fragment_product_list.*
@@ -32,6 +34,8 @@ class ProductListFragment : BaseFragment(), ProductListView {
     private fun initialiseAdapter() {
         product_recycler.layoutManager = LinearLayoutManager(context)
         product_recycler.adapter = ProductRecycler(mutableListOf())
+        val decoration = DividerItemDecoration(context, LinearLayoutManager.VERTICAL)
+        product_recycler.addItemDecoration(decoration)
     }
 
     override fun updateProductList(products: List<Product>) {
